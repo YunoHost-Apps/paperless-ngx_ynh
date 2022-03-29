7749940eec15b3f889d7e17c35a67a877af0caa2
Tagadda <36127788+Tagadda@users.noreply.github.com>
2022-03-29 15:50:14 +0000

    [wip] Initial release

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,12 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="redis-tools redis-server postgresql postgresql-contrib python3 python3-pip python3-dev python3-venv imagemagick fonts-liberation optipng gnupg libpq-dev libmagic-dev mime-support"
+ocr_pkg_dependencies="unpaper ghostscript icc-profiles-free qpdf liblept5 libxml2 pngquant zlib1g tesseract-ocr"
+raspberry_pkg_dependencies="libatlas-base-dev libxslt1-dev"
+
+timezone=$(cat /etc/timezone)
+main_domain=$(cat /etc/yunohost/current_host)
 
 #=================================================
 # PERSONAL HELPERS
