f2bd90492ab675c7513595fe3a15d101bd6ac6ff
Fabian Wilkens <wilkens.fabian+github@gmail.com>
2022-12-30 19:30:56 +0100

    Update to 1.11.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-#pkg_dependencies="redis-tools redis-server postgresql postgresql-contrib python3 python3-pip python3-dev python3-venv imagemagick fonts-liberation optipng gnupg libpq-dev libmagic-dev mime-support"
-pkg_dependencies="build-essential postgresql postgresql-contrib python3 python3-pip python3-dev python3-venv imagemagick fonts-liberation optipng gnupg libpq-dev libmagic-dev mime-support libzbar0 poppler-utils default-libmysqlclient-dev"
+pkg_dependencies="python3 python3-pip python3-dev python3-venv default-libmysqlclient-dev fonts-liberation imagemagick gnupg libpq-dev libmagic-dev mime-support libzbar0 poppler-utils postgresql postgresql-contrib "
 ocr_pkg_dependencies="unpaper ghostscript icc-profiles-free qpdf liblept5 libxml2 pngquant zlib1g tesseract-ocr"
 raspberry_pkg_dependencies="libatlas-base-dev libxslt1-dev"
 
