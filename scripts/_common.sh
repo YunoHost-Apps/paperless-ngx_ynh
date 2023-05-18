b61457b32ab9f0b1838f33dc4f8501c7d187bf26
Tagadda <36127788+Tagadda@users.noreply.github.com>
2023-05-18 09:16:36 +0000

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="python3 python3-pip python3-dev python3-venv default-libmysqlclient-dev fonts-liberation imagemagick gnupg libpq-dev libmagic-dev mime-support libzbar0 poppler-utils postgresql postgresql-contrib "
-#REMOVEME? ocr_pkg_dependencies="unpaper ghostscript icc-profiles-free qpdf liblept5 libxml2 pngquant zlib1g tesseract-ocr"
-#REMOVEME? raspberry_pkg_dependencies="libatlas-base-dev libxslt1-dev"
-
 timezone=$(cat /etc/timezone)
 main_domain=$(cat /etc/yunohost/current_host)
 
