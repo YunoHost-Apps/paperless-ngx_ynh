c95c56eb98a8746a235804ac6da461971532a645
Tagadda <36127788+Tagadda@users.noreply.github.com>
2023-05-18 09:01:25 +0000

    convert to v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,9 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3 python3-pip python3-dev python3-venv default-libmysqlclient-dev fonts-liberation imagemagick gnupg libpq-dev libmagic-dev mime-support libzbar0 poppler-utils postgresql postgresql-contrib "
-ocr_pkg_dependencies="unpaper ghostscript icc-profiles-free qpdf liblept5 libxml2 pngquant zlib1g tesseract-ocr"
-raspberry_pkg_dependencies="libatlas-base-dev libxslt1-dev"
+#REMOVEME? pkg_dependencies="python3 python3-pip python3-dev python3-venv default-libmysqlclient-dev fonts-liberation imagemagick gnupg libpq-dev libmagic-dev mime-support libzbar0 poppler-utils postgresql postgresql-contrib "
+#REMOVEME? ocr_pkg_dependencies="unpaper ghostscript icc-profiles-free qpdf liblept5 libxml2 pngquant zlib1g tesseract-ocr"
+#REMOVEME? raspberry_pkg_dependencies="libatlas-base-dev libxslt1-dev"
 
 timezone=$(cat /etc/timezone)
 main_domain=$(cat /etc/yunohost/current_host)
